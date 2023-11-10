aeca58daa6718a889e1eeebaec55f54147edcadc
OOI JIA YI <ooijiayi93@gmail.com>
2023-11-10 10:35:35 +0800

    DFSonBST solution

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,20 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function searchNode(curr: BinaryNode<number>, needle: number): boolean {
+    // define base case
+    if (!curr) return false;
 
+    if (curr.value === needle) return true;
+
+    if (curr.value > needle && curr.left) {
+        return searchNode(curr.left, needle);
+    }
+
+    if (curr.value < needle && curr.right) {
+        return searchNode(curr.right, needle);
+    }
+
+    return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return searchNode(head, needle);
 }
